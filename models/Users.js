d47442a9a72defb89b80de69f76e7ce213c54166
AliAhmed-8113 <ali2308113@miuegypt.edu.eg>
2025-06-14 03:36:03 +0300

    Create Users.js
    Added Users schema

---
+++ models/Users.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['Customer', 'Admin'], 
+        default: 'Customer' 
+    }
+}, {
+    timestamps: true
+});
+
+
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
